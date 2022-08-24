6b6ea81c0f02738f4631937bf881b29866b2b439
VIDYASHREE-HN <84257310+VIDYASHREE-HN@users.noreply.github.com>
2022-08-24 12:34:38 +0530

    Update leap.py

--- leap.py
+++ leap.py
@@ -1,6 +1,6 @@
 class leap(object):
     def year(self,a):
-        # year= int(input("Enter the year-"))
+        year= int(input("Enter the year-2022"))
         if (a % 400 == 0) and (a % 100 == 0):
             # print(f"{a} ia a leap year")
             return a
