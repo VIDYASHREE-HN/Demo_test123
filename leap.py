7e7b72a964e2e7456d8aab42010a49d9eeebaea3
VIDYASHREE-HN <84257310+VIDYASHREE-HN@users.noreply.github.com>
2022-10-06 12:13:25 +0530

    Update leap.py

--- leap.py
+++ leap.py
@@ -2,11 +2,14 @@
 #     def year(self,a):
 year= 2021
 if (a % 400 == 0) and (a % 100 == 0):
-            # print(f"{a} ia a leap year")
-        return a
+            print(f"{a} ia a leap year")
+#         return a
 elif (a % 4 == 0) and (a % 100 != 0):
             print(f"{a} is a leap year")
-        return a  
+#         return a  
+else:
+            print(f"{year} is not a leap year")
+            
         
 #f=leap()
 #print(f.year(2020))
